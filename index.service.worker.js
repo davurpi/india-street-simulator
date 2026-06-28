4b6f71488dc090cd045d3b343865acb7c52d26d6
davurpi <davurpi@gmail.com>
2026-06-28 13:13:16 +0200

    Update: global multiplayer relay system

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1782643228|5626587';
+const CACHE_VERSION = '1782645184|4708877';
 /** @type {string} */
 const CACHE_PREFIX = 'India Street Sim-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
